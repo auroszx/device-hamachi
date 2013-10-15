dd1796e835ca519522afba5e40b9c3e819594b82
Carlos Sanchez <cargabsj175@gamil.com>
2013-10-15 09:44:13 -0430

    added 7k_handset.kl, AndroidBoardVedor.mk, surf_keypad.kcm, surf_keypad.kl, vold.fstab.

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/alcatel/4012a/AndroidBoardVendor.mk
+include device/alcatel/4012a/AndroidBoardVendor.mk
